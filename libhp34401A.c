29515318ac7e648c7fba65c88adc05b47eace8ce
squadtak <40524603+squadtak@users.noreply.github.com>
2026-06-22 23:08:01 +0900

    Add files via upload

--- libhp34401A.c
+++ libhp34401A.c
@@ -1,5 +1,5 @@
 /*
- libhp34401A Ver 1.0 2026-06-22
+ libhp34401A Ver 1.1 2026-06-22
  (c)2026 squad
 */
 
@@ -16,11 +16,21 @@ int is34401A(struct sp_port *port)
 	if(hp34401ASendCommandWithRead(port, "*IDN?", 300000, &buf) != 0) return -1;
 
 	char *model_pos = NULL;
-	if((model_pos = strchr((char *)buf, ',')) == NULL) return -2;
-	if(strtok(model_pos + 1, ",") == NULL) return -3;
+	if((model_pos = strchr((char *)buf, ',')) == NULL){
+		free(buf);
+		return -2;
+	}
+
+	model_pos += 1; // ,
+
+	if(strtok(model_pos, ",") == NULL){
+		free(buf);
+		return -3;
+	}
 
 	int ret = 0;
-	if(strstr(model_pos + 1, "34401A") - (model_pos + 1) == 0 && strlen(model_pos + 1) == strlen("34401A")) ret = 1;
+
+	if(strstr(model_pos, "34401A") - model_pos == 0 && strlen(model_pos) == strlen("34401A")) ret = 1;
 
 	free(buf);
 	return ret;
@@ -28,8 +38,9 @@ int is34401A(struct sp_port *port)
 
 int hp34401ASendCommandWithRead(struct sp_port *port, char *command, size_t delay_read_data, char **readData)
 {
-	int ret = -1;
-	if(hp34401ASendCommand(port, command)) goto err0;
+	int ret = 0;
+
+	if(hp34401ASendCommand(port, command)) return -1;
 
 	if(delay_read_data) usleep(delay_read_data);
 
@@ -38,6 +49,7 @@ int hp34401ASendCommandWithRead(struct sp_port *port, char *command, size_t dela
 	*readData = NULL;
 	size_t readDataLen = 0;
 	do{
+
 		char *read_buf_tmp;
 
 		if((read_buf_tmp = realloc(*readData, readDataLen + 1)) == NULL){
@@ -45,34 +57,36 @@ int hp34401ASendCommandWithRead(struct sp_port *port, char *command, size_t dela
 				free(*readData);
 				*readData = NULL;
 			}
-			ret = -2;
-			goto err0;
+			return -2;
 		}
-
 		*readData = read_buf_tmp;
+
 retry:
 		int rett = 0;
 		if((rett = sp_blocking_read(port, *readData + readDataLen, 1, 10)) < 1){
+
 			if(rett == 0){
+
 				if(!retryFlag){
 					ret = 1;
 					break;
 				}
+
 				retryFlag--;
 				goto retry;
+
 			}else{
+
 				free(*readData);
 				*readData = NULL;
-				ret = -3;
-				goto err0;
+				return -3;
+
 			}
 		}
-
 
 		if(*(*readData + readDataLen) == '\r'){
 			char buf[1];
 			sp_blocking_read(port, buf, 1, 1000);
-			ret = 0;
 			break;
 		}
 
@@ -81,19 +95,16 @@ retry:
 
 	*(*readData + readDataLen) = '\0';
 
-err0:
 	return ret;
 }
 
 int hp34401ASendCommand(struct sp_port *port, char *command)
 {
-	//printf("[INFO]send command:%s\n", command);
-
 	char sendBuf[strlen(command)];
 	memcpy(sendBuf, command, strlen(command));
 	sendBuf[strlen(command)] = '\n';
 
-	if(sp_blocking_write(port, sendBuf, strlen(command) + 1, 1000) < 0) return -1;
+	if(sp_blocking_write(port, sendBuf, strlen(command) + 1, 1000) < strlen(command) + 1) return -1;
 	usleep(20000);
 
 	return 0;
@@ -124,7 +135,7 @@ int hp34401APrintText(struct sp_port *port, size_t scrollDelay, char *format, ..
 
 	}else{
 		memcpy(commandBuf + strlen("DISP:TEXT \""), buf, strLen);
-		memcpy(commandBuf + strlen("DISP:TEXT \"") + strLen, "\"\r", strlen("\"\r"));
+		memcpy(commandBuf + strlen("DISP:TEXT \"") + strLen, "\"\r\0", strlen("\"\r") + 1);
 		if(hp34401ASendCommand(port, commandBuf)) return -3;
 	}
 
